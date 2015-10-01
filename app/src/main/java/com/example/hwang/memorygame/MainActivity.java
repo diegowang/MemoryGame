d83020c04fd99c88721488b1a0c5ec5fe749ff92
Diego Wang <diegowang@yahoo.com>
2015-10-01 12:11:35 -0400

    The first set up.

--- app/src/main/java/com/example/hwang/memorygame/MainActivity.java
+++ app/src/main/java/com/example/hwang/memorygame/MainActivity.java
@@ -1,37 +1,31 @@
 package com.example.hwang.memorygame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Make a button from the button in our layout
+        Button button =(Button) findViewById(R.id.button);
+
+        //Make each it listen for clicks
+        button.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
